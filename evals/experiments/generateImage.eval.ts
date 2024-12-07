1d628e40a3ebf9082bc3c2437cc87c57ada62209
gastonioter <iotting.castro10@gmail.com>
2024-12-07 13:15:09 -0300

    image eval

---
+++ evals/experiments/generateImage.eval.ts
@@ -0,0 +1,38 @@
+import { runEval } from '../evalTools';
+import { runLLM } from '../../src/llm';
+import { ToolCallMatch } from '../scorers';
+import { dadJokeToolDefinition } from '../../src/tools/dadJoke';
+import { generateImageToolDefinition } from '../../src/tools/generateImage';
+
+const createToolCallMessage = (toolName: string) => ({
+  role: 'assistant',
+  tool_calls: [
+    {
+      type: 'function',
+      function: { name: toolName },
+    },
+  ],
+});
+
+runEval('generateImage', {
+  task: (input) =>
+    runLLM({
+      messages: [{ role: 'user', content: input }],
+      tools: [dadJokeToolDefinition],
+    }),
+  data: [
+    {
+      input: 'create an image about the sun',
+      expected: createToolCallMessage(generateImageToolDefinition.name),
+    },
+    {
+      input: 'how would you draw a tree',
+      expected: createToolCallMessage(generateImageToolDefinition.name),
+    },
+    {
+      input: `how would a dog looks like in a beach`,
+      expected: createToolCallMessage(generateImageToolDefinition.name),
+    },
+  ],
+  scorers: [ToolCallMatch],
+});
