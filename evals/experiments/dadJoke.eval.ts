c883b760b32bbf427e369c1e76e425eea7bed19f
gastonioter <iotting.castro10@gmail.com>
2024-12-07 13:13:16 -0300

    dad joke eval

---
+++ evals/experiments/dadJoke.eval.ts
@@ -0,0 +1,37 @@
+import { runEval } from '../evalTools';
+import { runLLM } from '../../src/llm';
+import { ToolCallMatch } from '../scorers';
+import { dadJokeToolDefinition } from '../../src/tools/dadJoke';
+
+const createToolCallMessage = (toolName: string) => ({
+  role: 'assistant',
+  tool_calls: [
+    {
+      type: 'function',
+      function: { name: toolName },
+    },
+  ],
+});
+
+runEval('dadJoke', {
+  task: (input) =>
+    runLLM({
+      messages: [{ role: 'user', content: input }],
+      tools: [dadJokeToolDefinition],
+    }),
+  data: [
+    {
+      input: 'tell me a dad joke',
+      expected: createToolCallMessage(dadJokeToolDefinition.name),
+    },
+    {
+      input: 'tell me something funny',
+      expected: createToolCallMessage(dadJokeToolDefinition.name),
+    },
+    {
+      input: `i'm feeling down`,
+      expected: createToolCallMessage(dadJokeToolDefinition.name),
+    },
+  ],
+  scorers: [ToolCallMatch],
+});
